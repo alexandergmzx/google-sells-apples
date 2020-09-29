b361e6f291f61a3a290de8cb8e33440a1d95d223
Alexandergmzx <alexandergmzx@gmail.com>
2020-09-29 14:38:16 -0500

    [run.py] the image name was not existent in the descriptions files

--- run.py
+++ run.py
@@ -9,11 +9,15 @@ dict_o_dicts = {}
 
 for feed in os.listdir("../supplier-data/descriptions/"):
         dict_o_dicts[feed] = {}
+        img_num = 1
         with open(("../supplier-data/descriptions/"+feed), mode='r', encoding='UTF-8') as file:
                 dict_o_dicts[feed]["name"] = file.readline().rstrip()
                 dict_o_dicts[feed]["weigth"] = int(re.search(r'\d+', file.readline().rstrip()).group())
                 dict_o_dicts[feed]["description"] = file.readline().rstrip()
-                dict_o_dicts[feed]["image_name"] = file.readline().rstrip()
+                if img_num < 10:
+                        dict_o_dicts[feed]["image_name"] = "0"+str(img_num)+".jpeg"
+                else:
+                	dict_o_dicts[feed]["image_name"] = "00"+str(img_num)+".jpeg"
         response = requests.post(url, data=dict_o_dicts[feed])
         response.raise_for_status()
 
