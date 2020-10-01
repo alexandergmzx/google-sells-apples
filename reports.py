f01ba51dc104cec05d091fe19f561ab4085de81f
Alexandergmzx <alexandergmzx@gmail.com>
2020-09-30 21:11:06 -0500

    [reports] The reports script make a beautiful PDF v1

--- reports.py
+++ reports.py
@@ -15,11 +15,11 @@ from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 
-def generate(filename, title):
+def generate(filename, title, additional_info):
   styles = getSampleStyleSheet()
   report = SimpleDocTemplate(filename)
   report_title = Paragraph(title, styles["h1"])
-  report_info = Paragraph("additional_info", styles["BodyText"])
+  report_info = Paragraph(additional_info, styles["BodyText"])
   empty_line = Spacer(1,20)
   report.build([report_title, empty_line, report_info, empty_line])
 
@@ -29,27 +29,40 @@ def load_data(filename):
     data = json.load(json_file)
   return data
 
+def process_data(data):
+  '''
+  Takes the json dictionary and turns it into a string list
+  that is rady and available to process into the report.
+  Like : "name: *Fruit name*
+          weigth: *Fruit weigth* lbs"
+  '''
+  #data_list = []
+  data_string = ""
+  for value in data.values():
+    data_string += "name: " + value["name"] + "<br/>" + \
+                  "weigth: " + str(value["weigth"]) + " lbs" + "<br/>" + "<br/>"
+    #data_list.append(data_string)
+  return data_string
+
 def main(argv):
   """Process the JSON data and generate a full report out of it."""
   data = load_data("../dict_o_dicts.json")
-  #print(data)
-  # TODO: turn this into a PDF report
+  processed = process_data(data)  
+  print(processed)
 
   titulo = "Processed Update on " + datetime.date.today().strftime("%B %d, %Y") # Add the date in international format
   # Like: March 11, 2020
 
   reports.generate("../tmp/processed.pdf", 
-    titulo)
+    titulo, processed)
 
-  sender = "sender@example.com"
+  sender = "automation@example.com"
   receiver = "{}@example.com".format(os.environ.get('USER'))
-  subject = "Sales summary for last month"
-  #body = data
+  subject = "Upload Completed - Online Fruit Store"
+  body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
 
-  message = emails.generate(sender, receiver, subject, "body","../tmp/processed.pdf")
+  message = emails.generate(sender, receiver, subject, body,"../tmp/processed.pdf")
   #emails.send(message)
-  # TODO: send the PDF report as an email attachment
-
 
 if __name__ == "__main__":
   main(sys.argv)
