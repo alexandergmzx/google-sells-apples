93697650a044f0dc4ad185646be83302e7eef8d7
Alexandergmzx <alexandergmzx@gmail.com>
2020-09-30 23:16:29 -0500

    [feat] Now healthchecks can send emails Big Changes made to health checks and to some of the other scripts in order to make everythong work as planned

--- emails.py
+++ emails.py
@@ -27,6 +27,17 @@ def generate(sender, recipient, subject, body, attachment_path):
 
   return message
 
+def generate2(sender, recipient, subject, body):
+  """Creates an email with an attachement."""
+  # Basic Email formatting
+  message = email.message.EmailMessage()
+  message["From"] = sender
+  message["To"] = recipient
+  message["Subject"] = subject
+  message.set_content(body)
+
+  return message
+
 def send(message):
   """Sends the message to the configured SMTP server."""
   mail_server = smtplib.SMTP('localhost')
--- health_check.py
+++ health_check.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import shutil
 import psutil
+import os
+import emails
+import socket
+import sys
 
 def check_cpu_usage():
 	usage = psutil.cpu_percent(1)
@@ -13,12 +17,32 @@ def check_disk_usage(disk):
 
 def check_memory_usage():
     available = psutil.virtual_memory()[1]/2.**20
-    return available > 500
+    return available > 524.288
 
-if not check_cpu_usage() 
-    or not check_disk_usage("/") 
-    or not check_memory_usage():
-	print("ERROR !")	
-else:
-	print("Everything is OK :)")
+def main(argv):	
+	"""Process the JSON data and generate a full report out of it."""
+	print(socket.gethostbyname(socket.gethostname()))
+	subject_line = ""
+	if not check_cpu_usage() :
+		subject_line = "Error - CPU usage is over 80%"
+	elif not check_disk_usage("/") :
+		subject_line = "Error - Available disk space is less than 20%"
+	elif not check_memory_usage() :
+		subject_line = "Error - Available memory is less than 500MB"
+	elif not (socket.gethostbyname(socket.gethostname()) == "127.0.0.1"):
+		subject_line = "Error - localhost cannot be resolved to 127.0.0.1"
+	else:
+		print("Everything is OK :)")
 
+	if not (subject_line == ""):
+		print(subject_line)
+		sender = "automation@example.com"
+		receiver = "{}@example.com".format(os.environ.get('USER'))
+		subject = subject_line
+		body = "Please check your system and resolve the issue as soon as possible."
+
+		message = emails.generate2(sender, receiver, subject, body)
+		#emails.send(message)
+
+if __name__ == "__main__":
+  main(sys.argv)
