350a0bf3922ec4cd45956d9e124e4087ddbbbea5
Alexandergmzx <alexandergmzx@gmail.com>
2020-09-30 23:48:39 -0500

    [run.py] URL updated

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import requests
 import json
 import re
 
-url = "http://??/fruits/"
+url = "http://34.122.175.55/fruits/"
 dict_o_dicts = {}
 
 for feed in os.listdir("../supplier-data/descriptions/"):
